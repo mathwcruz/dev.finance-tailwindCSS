e9d271c677d45753f8b3d27b188a68171cb462ea
Matheus da Cruz <matheuswachcruz@gmail.com>
2021-01-26 20:35:19 -0300

    feat: add register form and modals

--- public/js/login.js
+++ public/js/login.js
@@ -1,25 +1,74 @@
-//Validação de login
-const buttonOpenModal = document.getElementById('buttonModal');
+//Função para abrir o modal
+function openModal(modal) {
+  modal.classList.remove('modal-inactive');
+  modal.classList.add('active');
+};
+
+//Função para fechar o modal 
+function closeModal(modal) {
+  modal.classList.remove('active');
+  modal.classList.add('modal-inactive');
+};
+
+//Função para mudar o ícone de alerta
+function changeAlertIcon(icon) {
+  icon.classList.remove('fa-exclamation-triangle');
+  icon.classList.add('fa-check');
+  icon.classList.add('text-green-700');
+};
+
+//Função para fechar o modal contendo mensagem de sucesso ao cadastar
+function buttonCloseModalSuccess(button) {
+  button.textContent = "Logar";
+  button.classList.add('hover:bg-white');
+
+  button.addEventListener('click', () => {
+    window.location.href = "../views/sign-in.html";
+  });
+};
+
+//Variáveis globais
+const buttonOpenModalLogin = document.getElementById('buttonModal');
 let modalOverlay = document.getElementById('modal-overlay');
+let AlertIcon = document.querySelector('[data-icon-alert]');
+let buttonCloseModal = document.getElementById('try-again-button');
 
-buttonOpenModal.addEventListener('click', () => {
-  const user = document.getElementById('user');
-  const password = document.getElementById('password');
+//Validação de login
+buttonOpenModalLogin.addEventListener('click', () => {
+  const inputsLogin = document.querySelectorAll('.input-mark');
+  inputsLogin.forEach((input) => {
+    if (input.value === "") {
+      openModal(modalOverlay);
+    } else {
+      input.value = "";
+      //VER PQ AO PREECHER SOMENTE UM CAMPO, ELE JA VALIDA O LOGIN/CADASTRO
+      // window.location.href = "../views/index.html";
+    };
+  });
+});
 
-  if (user.value === "" || password.value === "") {
-    modalOverlay.classList.remove('modal-inactive');
-    modalOverlay.classList.add('active');
-  } else {
-    user.value = "";
-    password.value = "";
-    window.location.href = '../views/index.html';
-  };
+//Validação de cadastro
+const buttonOpenModalRegister = document.querySelector('[data-button-register]');
+const termsAndPolicy = document.getElementById('polycy&terms');
 
-})
+buttonOpenModalRegister.addEventListener('click', () => {
+  const inputsRegister = document.querySelectorAll('.input-modal');
+  inputsRegister.forEach((input) => {
+    if (input.value === "" || termsAndPolicy.checked === false) {
+      openModal(modalOverlay);
+    } else {
+      const titleModalSuccess = document.querySelector('[data-title-modal-success]');
+      const paragraphModalSuccess = document.querySelector('[data-paragraph-modal-success]');
+      changeAlertIcon(AlertIcon);
 
-const buttonCloseModal = document.getElementById('try-again-button');
+      input.value = "";
 
-buttonCloseModal.addEventListener('click', () => {
-  modalOverlay.classList.remove('active');
-  modalOverlay.classList.add('modal-inactive');
+      openModal(modalOverlay);
+      titleModalSuccess.textContent = "Cadastro efetuado com sucesso!";
+      paragraphModalSuccess.textContent = "Clique no botão abaixo para seguir para logar-se";
+      buttonCloseModalSuccess(buttonCloseModal);
+
+      //VER PQ AO PREECHER SOMENTE UM CAMPO, ELE JA VALIDA O LOGIN/CADASTRO
+    };
+  });
 });
