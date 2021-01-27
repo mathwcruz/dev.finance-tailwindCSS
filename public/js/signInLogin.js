cb755b1c88716fd9bea6758fa5d4a5f914b64924
Matheus da Cruz <matheuswachcruz@gmail.com>
2021-01-27 16:52:32 -0300

    docs: add a validation input for the sign-in page

--- public/js/signInLogin.js
+++ public/js/signInLogin.js
@@ -19,15 +19,28 @@ function buttonCloseModalError(button, modal) {
 
 //Validação de login
 buttonOpenModalLogin.addEventListener('click', () => {
-  const inputsLogin = document.querySelectorAll('.input-sign-in-page');
-  inputsLogin.forEach((input) => {
-    if (input.value === "") {
-      openModal(modalOverlay);
-      buttonCloseModalError(buttonCloseModalLogin, modalOverlay)
-    } else {
-      input.value = "";
-        //VER PQ AO PREECHER SOMENTE UM CAMPO, ELE JA VALIDA O LOGIN/CADASTRO
-        window.location.href = "../views/index.html";
-    };
-  });
+  const inputUser = document.getElementById('user');
+  const inputPassword = document.getElementById('password');
+
+  //  MELHORAR ISSO
+  if (inputUser.value === "" || inputPassword.value === "") {
+    openModal(modalOverlay);
+    buttonCloseModalError(buttonCloseModalLogin, modalOverlay)
+  } else {
+    inputUser.value = "";
+    inputPassword.value = "";
+    window.location.href = "../views/index.html";
+  };
+
+  // Fazer isso funcionar
+  // inputsLogin.forEach((input) => {
+  //   if (input.value === "") {
+  //     openModal(modalOverlay);
+  //     buttonCloseModalError(buttonCloseModalLogin, modalOverlay)
+  //   } else {
+  //     input.value = "";
+  //       //VER PQ AO PREECHER SOMENTE UM CAMPO, ELE JA VALIDA O LOGIN/CADASTRO
+  //       window.location.href = "../views/index.html";
+  //   };
+  // });
 });
