dc0d1197ca0f42259df5a3065471a4189c6bd6d5
Matheus da Cruz <matheuswachcruz@gmail.com>
2021-01-29 18:32:40 -0300

    style: tried to fix mask and reg ex problems

--- public/js/signUpLogin.js
+++ public/js/signUpLogin.js
@@ -5,46 +5,81 @@ import {
   buttonCloseModalSuccess,
   modalOverlay,
   AlertIcon,
-  buttonCloseModal
-} from './utils.js';
+  buttonCloseModal,
+} from "./utils.js";
 
-import { masks } from './masksAndRegEx.js';
+import { masks } from "./masksAndRegEx.js";
 
 //Máscara
 function maskReplace() {
-  const inputs = document.querySelectorAll('.mask').forEach(input => {
+  document.querySelectorAll(".mask").forEach((input) => {
     const field = input.name;
-    input.addEventListener('input', (e) => {
-      e.target.value = masks[field](e.target.value);
-    }, false);
+    input.addEventListener(
+      "input",
+      (e) => {
+        e.target.value = masks[field](e.target.value);
+      },
+      false
+    );
   });
-};
+}
 
 maskReplace();
 
 //Validação de cadastro
-const buttonOpenModalRegister = document.querySelector('[data-button-register]');
-const termsAndPolicy = document.getElementById('polycy&terms');
+const buttonOpenModalRegister = document.querySelector(
+  "[data-button-register]"
+);
+const termsAndPolicy = document.getElementById("polycy&terms");
 
-buttonOpenModalRegister.addEventListener('click', () => {
-  const inputsRegister = document.querySelectorAll('.input-modal');
+buttonOpenModalRegister.addEventListener("click", () => {
+  const inputsRegister = document.querySelectorAll(".input-modal");
+
+  //VER PQ NAO FUNCIONA
+  // for (let x = 0; x < inputsRegister.length; x++) {
+  //   if (inputsRegister[x].value === "" || termsAndPolicy.checked === false) {
+  //     openModal(modalOverlay);
+  //     buttonCloseModalError(buttonCloseModal, modalOverlay);
+  //   } else {
+  //     const titleModalSuccess = document.querySelector(
+  //       "[data-title-modal-success]"
+  //     );
+  //     const paragraphModalSuccess = document.querySelector(
+  //       "[data-paragraph-modal-success]"
+  //     );
+  //     changeAlertIcon(AlertIcon);
+
+  //     input.value = "";
+
+  //     openModal(modalOverlay);
+  //     titleModalSuccess.textContent = "Cadastro efetuado com sucesso!";
+  //     paragraphModalSuccess.textContent =
+  //       "Clique no botão abaixo para logar-se";
+  //     buttonCloseModalSuccess(buttonCloseModal);
+  //   }
+  // }
+
+  //VER PQ AO PREECHER SOMENTE UM CAMPO, ELE JA VALIDA O LOGIN/CADASTRO
   inputsRegister.forEach((input) => {
     if (input.value === "" || termsAndPolicy.checked === false) {
       openModal(modalOverlay);
-      buttonCloseModalError(buttonCloseModal, modalOverlay)
+      buttonCloseModalError(buttonCloseModal, modalOverlay);
     } else {
-      const titleModalSuccess = document.querySelector('[data-title-modal-success]');
-      const paragraphModalSuccess = document.querySelector('[data-paragraph-modal-success]');
+      const titleModalSuccess = document.querySelector(
+        "[data-title-modal-success]"
+      );
+      const paragraphModalSuccess = document.querySelector(
+        "[data-paragraph-modal-success]"
+      );
       changeAlertIcon(AlertIcon);
 
       input.value = "";
 
       openModal(modalOverlay);
       titleModalSuccess.textContent = "Cadastro efetuado com sucesso!";
-      paragraphModalSuccess.textContent = "Clique no botão abaixo para logar-se";
+      paragraphModalSuccess.textContent =
+        "Clique no botão abaixo para logar-se";
       buttonCloseModalSuccess(buttonCloseModal);
-
-      //VER PQ AO PREECHER SOMENTE UM CAMPO, ELE JA VALIDA O LOGIN/CADASTRO
-    };
+    }
   });
 });
